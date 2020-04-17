2eecc8275dc45386b0ca1e72aa577ea863c848ec
Akiyama Hiroki <hiroki.akiyama.tokyo@gmail.com>
2020-04-17 17:37:45 +0900

    とりあえず

--- code/rendering_book.R
+++ code/rendering_book.R
@@ -2,22 +2,18 @@ library(tidyverse)
 library(bookdown)
 library(here)
 
-if (file.exists("data_engineering.Rmd")){
-  file.remove("data_engineering.Rmd")
-}
-
-d <- dir(here("code/code_bookdown")) %>%
+d <- dir(here("code")) %>%
   tibble(file = .) %>%
   mutate(Rmd = str_detect(file, ".Rmd")) %>%
   filter(Rmd)
 d
 
 
-setwd(here("code/code_bookdown/"))
-d$file %>% render_book(preview = TRUE,
+setwd(here("code/"))
+d$file %>% render_book(
+  preview = TRUE,
   clean = TRUE,
   output_dir = here("output_book"),
   encoding = "UTF-8",
   config_file = c("_bookdown.yml")
 )
-
